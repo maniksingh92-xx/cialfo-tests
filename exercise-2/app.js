81b4d10c0c55bc53927d2ae248bdd95f04350692
maniksingh92 <maniksingh92@gmail.com>
2015-10-24 04:48:21 +0530

    finalize exercise 2

--- exercise-2/app.js
+++ exercise-2/app.js
@@ -1,98 +1,260 @@
-var topLeft = view.center - [600,300];
-var bottomRight = view.center + [600,300];
+(function () {
+  var background, centerCloud, clouds, lightning;
+  var children = project.activeLayer.children;
+  var lives = 3;
+  var ld = 1;
+  var correct = 0;
+  var placedSymbol = [];
 
-var background = new Path.Rectangle({
-  topLeft: topLeft,
-  bottomRight: bottomRight,
-  fillColor: {
-    gradient: {
-      stops: ['#b0e0e6', '#c0eeec']
-    },
-    origin: topLeft,
-    destination: bottomRight
+  //one rain drop
+  var r1 = new Path.Rectangle({
+    point: [0,0],
+    size: [1,10],
+    fillColor: new Color('white')
+  });
+  //make the rain drop into a symbol
+  var symbol = new Symbol(r1);
+  r1.remove();
+
+  initializeCanvas(view.center - [600, 300], view.center + [600, 300], "Skeptic", ["Doubtful", "Dubious", "Cynic"], ["Honorable", "Helpful", "Diligent"]);
+
+  function initializeCanvas(tl, br, mainWord, correctWords, incorrectWords) {
+    "use strict";
+
+    clouds = {};
+    correctWords = shuffleArray(correctWords);
+    incorrectWords = shuffleArray(incorrectWords);
+    var smallCoordinates = [[400, 100],[-400, -100],[0, 200],[0, -200],[400, -100],[-400, 100]];
+
+    background = new Path.Rectangle({
+      topLeft: tl,
+      bottomRight: br,
+      fillColor: {
+        gradient: {
+          stops: ['#b0e0e6', '#c0eeec']
+        },
+        origin: tl,
+        destination: br
+      }
+    });
+    background.name = "background";
+
+    lightning = new Raster('lightning');
+    lightning.position = view.center + [200,-110];
+    lightning.name = 'lightning1';
+    lightning.opacity = 0;
+
+    generateClouds(mainWord, correctWords, incorrectWords, smallCoordinates);
+
   }
-});
 
-//Center Cloud
-var largeCloud = new Raster('bigCloud');
-largeCloud.position = view.center;
-var largeText = new PointText(new Point(10, 0));
-largeText.characterStyle = {
-  fontSize:36,
-  fillColor:"#ed1c24",
-  font:"Arial"
-};
-largeText.position = largeCloud.position - [50,-15];
-largeText.content = "Skeptic";
+  function generateClouds(mainWord, correctWords, incorrectWords, smallCoordinates) {
+    "use strict";
 
-//Top left cloud
-var topLeftCloud = new Raster('cloud1');
-topLeftCloud.position = view.center - [400,100];
-var topLefttext = new PointText(new Point(10, 0));
-topLefttext.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-topLefttext.position = topLeftCloud.position - [40,-15];
-topLefttext.content = "Cynic";
+    while(correctWords.length + incorrectWords.length != 0) {
+      var rand = Math.floor(Math.random() * 2);
+      var cloudType = (correctWords.length + incorrectWords.length)%2 ? "cloud1" : "cloud2";
+      if(rand == 0) {
 
-//Bottom Right Cloud
-var bottomRightCloud = new Raster('cloud1');
-bottomRightCloud.position = view.center + [400,100];
-var bottomRightText = new PointText(new Point(10, 0));
-bottomRightText.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-bottomRightText.position = bottomRightCloud.position - [40,-15];
-bottomRightText.content = "Doubtful";
+        if(correctWords.length != 0) clouds[correctWords[correctWords.length - 1]] = new SmallCloud(correctWords.pop(), smallCoordinates.pop(), cloudType, true);
+        else clouds[incorrectWords[incorrectWords.length - 1]] = new SmallCloud(incorrectWords.pop(), smallCoordinates.pop(), cloudType, false);
+      } else {
+        if(incorrectWords.length != 0) clouds[incorrectWords[incorrectWords.length - 1]] = new SmallCloud(incorrectWords.pop(), smallCoordinates.pop(), cloudType, false);
+        else clouds[correctWords[correctWords.length - 1]] = new SmallCloud(correctWords.pop(), smallCoordinates.pop(), cloudType, true);
+      }
+    }
 
-//Bottom cloud
-var bottomCloud = new Raster('cloud2');
-bottomCloud.position = view.center + [0,200];
-var bottomText = new PointText(new Point(10, 0));
-bottomText.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-bottomText.position = bottomCloud.position - [25,-15];
-bottomText.content = "Helpful";
+    centerCloud = new CenterCloud(mainWord);
+    centerCloud.name = "centerCloud";
+  }
 
-//Top cloud
-var topCloud = new Raster('cloud1');
-topCloud.position = view.center - [0,200];
-var topText = new PointText(new Point(10, 0));
-topText.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-topText.position = topCloud.position - [40,-15];
-topText.content = "Dubious";
+  function CenterCloud(text) {
+    this.raster = new Raster('bigCloud');
+    this.raster.position = view.center;
+    this.text = new PointText(new Point(10, 0));
+    this.text.characterStyle = {
+      fontSize: 36,
+      fillColor: "#ed1c24",
+      font: "Arial"
+    };
+    this.text.position = this.raster.position - [50, -15];
+    this.text.content = text;
 
-//Top right cloud
-var topRightCloud = new Raster('cloud2');
-topRightCloud.position = view.center + [400,-100];
-var topRightText = new PointText(new Point(10, 0));
-topRightText.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-topRightText.position = topRightCloud.position - [40,-15];
-topRightText.content = "Honorable";
+    this.getPosition = function () {
+      "use strict";
+      return this.raster.position;
+    }
+  }
 
-//Bottom Left Cloud
-var bottomLeftCloud = new Raster('cloud1');
-bottomLeftCloud.position = view.center + [-400,100];
-var bottomLeftText = new PointText(new Point(10, 0));
-bottomLeftText.characterStyle = {
-  fontSize:16,
-  fillColor:"#777777",
-  font:"Arial"
-};
-bottomLeftText.position = bottomLeftCloud.position - [40,-15];
-bottomLeftText.content = "Eagerness";
+  function SmallCloud(text, pos, cloud, isCorrect) {
+    "use strict";
+    var self = this;
+    self.tried = false;
+
+    self.link = new Path({
+      segments: [view.center + pos, [view.center.x, view.center.y]]
+    });
+    self.link.strokeColor = '#ffffff';
+    self.link.strokeWidth = 4;
+    self.link.dashArray = [20, 12];
+    self.link.visible = false;
+    self.link.name = text + "link";
+
+    self.isCorrect = isCorrect;
+    self.raster = new Raster(cloud);
+    self.raster.position = view.center + pos;
+    self.raster.name = text + "cloud";
+
+    self.text = new PointText(new Point(10, 0));
+    self.text.characterStyle = {
+      fontSize: 16,
+      fillColor: "#777777",
+      font: "Arial"
+    };
+    self.text.position = self.raster.position - [25, -15];
+    self.text.content = text;
+
+    self.mark = self.isCorrect ? new Raster('checkMark') : new Raster('crossMark');
+    //self.mark = new Raster('checkMark');
+    self.mark.position = self.text.position - [50, 0];
+    self.mark.visible = false;
+    self.mark.name = text + "mark";
+
+    self.raster.onClick = function (event) {
+      children[self.mark.name].visible = true;
+      if(self.tried == false) {
+        self.tried = true;
+        self.raster.onClick = null;
+        if(self.isCorrect) {
+          correct++;
+          children[self.link.name].visible = true;
+          children['background'].fillColor = {
+            gradient: {
+              stops: ['#b0e0e6', '#c0eeec']
+            },
+            origin: view.center - [600, 300],
+            destination: view.center + [600, 300]
+          };
+          if(children['lightning1'].visible != undefined) {
+            children['lightning1'].visible = false;
+          }
+          if(correct == 3) {
+            var gameWon = new Path.Rectangle({
+              topLeft: view.center - [600, 300],
+              bottomRight: view.center + [600, 300],
+              fillColor: {
+                gradient: 'black',
+                origin: view.center - [600, 300],
+                destination: view.center + [600, 300]
+              }
+            });
+            gameWon.opacity=0;
+            var winText = new PointText(new Point(10, 0));
+            winText.characterStyle = {
+              fontSize: 16,
+              fillColor: "#ffffff",
+              font: "Arial"
+            };
+            winText.position = view.center + [-25, -15];
+            winText.content = "Game Won!";
+            winText.opacity = 0;
+            view.onFrame = function () {
+              gameWon.opacity += 0.03;
+              winText.opacity += 0.03;
+            }
+
+
+          }
+        } else {
+          lives--;
+          if(lives <= 2) {
+            children['background'].fillColor = {
+              gradient: {
+                stops: [['#85a39f', 0.2], '#0e2f44']
+              },
+              origin: view.center - [600, 300],
+              destination: view.center + [600, 300]
+            };
+
+            view.onFrame = function() {
+              if(ld == 1) {
+                if(lightning.opacity >= 1) {
+                  ld *= -1;
+                  while(lightning.opacity < 1) lightning.opacity -= 0.01;
+                }
+                else lightning.opacity += 0.01;
+
+              } else {
+
+                if(lightning.opacity <= 0) {
+                  ld *= -1;
+                  while(lightning.opacity > 0) lightning.opacity += 0.01;
+                }
+                else lightning.opacity -= 0.01;
+              }
+            };
+
+
+
+          }
+
+          if(lives ==  1) {
+            lightning.opacity = 1;
+            view.onFrame = function() {
+              rain();
+            }
+          }
+
+          if(lives == 0) {
+            new Path.Rectangle({
+              topLeft: view.center - [600, 300],
+              bottomRight: view.center + [600, 300],
+              fillColor: {
+                gradient: 'black',
+                origin: view.center - [600, 300],
+                destination: view.center + [600, 300]
+              }
+            });
+
+            var overText = new PointText(new Point(10, 0));
+            overText.characterStyle = {
+              fontSize: 16,
+              fillColor: "#ffffff",
+              font: "Arial"
+            };
+            overText.position = view.center + [-25, -15];
+            overText.content = "Game Over.";
+          }
+        }
+      }
+    }
+  }
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+  function rain() {
+    "use strict";
+    var group = new Group();
+
+    //make 150 symbols (drops), place them at random x position
+    for (var i = 0; i < 20; i++){
+      placedSymbol[i] = symbol.place();
+      placedSymbol[i].position = Point.random() * view.size;
+      group.addChild(placedSymbol[i]);
+    }
+    window.setTimeout(function() {
+      group.visible = false;
+    }, 100);
+
+
+  }
+
+})();
